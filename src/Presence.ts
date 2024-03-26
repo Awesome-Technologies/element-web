cc4e5ea494881f148b65dd2a653682292d36a270
Michael Albert <michael.albert@awesome-technologies.de>
2024-03-26 08:55:43 +0100

    Performs an auto (soft-)logout after the set duration of inactivity
    Change-Id: I85784bdd0a8cf12b3c28d77dac59e7bd115000f5

---
+++ src/Presence.ts
@@ -0,0 +1,136 @@
+/*
+Copyright 2015, 2016 OpenMarket Ltd
+Copyright 2018 New Vector Ltd
+Copyright 2019 The Matrix.org Foundation C.I.C.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// ORIGINAL PATH
+// matrix-react-sdk/src/
+
+import { logger } from "matrix-js-sdk/src/logger";
+import { MatrixClientPeg } from "matrix-react-sdk/src/MatrixClientPeg";
+import dis from "matrix-react-sdk/src/dispatcher/dispatcher";
+import Timer from "matrix-react-sdk/src/utils/Timer";
+import { ActionPayload } from "matrix-react-sdk/src/dispatcher/payloads";
+import { softLogout } from "matrix-react-sdk/src/Lifecycle";
+import SettingsStore from "matrix-react-sdk/src/settings/SettingsStore";
+
+// Time in ms after that a user is considered as unavailable/away
+const UNAVAILABLE_TIME_MS = 3 * 60 * 1000; // 3 mins
+
+enum State {
+    Online = "online",
+    Offline = "offline",
+    Unavailable = "unavailable",
+}
+
+class Presence {
+    private unavailableTimer: Timer | null = null;
+    private autoLogoutTimer: Timer | null = null;
+    private dispatcherRef: string | null = null;
+    private state: State | null = null;
+
+    /**
+     * Start listening the user activity to evaluate his presence state.
+     * Any state change will be sent to the homeserver.
+     */
+    public async start(): Promise<void> {
+        // load time from settings
+        const inactivityTimeOnLock = SettingsStore.getValue("inactivityTimeOnLock");
+        // set autoLogout timer to the value stored in the settings minus the three minutes of the unavailable timer
+        this.autoLogoutTimer = new Timer(inactivityTimeOnLock * 60 * 1000 - UNAVAILABLE_TIME_MS);
+
+        this.unavailableTimer = new Timer(UNAVAILABLE_TIME_MS);
+        // the user_activity_start action starts the timer
+        this.dispatcherRef = dis.register(this.onAction);
+        while (this.unavailableTimer) {
+            try {
+                await this.unavailableTimer.finished();
+                this.setState(State.Unavailable);
+
+                // wait until auto logout timer finished to soft logout
+                while (this.autoLogoutTimer) {
+                    try {
+                        await this.autoLogoutTimer.finished();
+                        // do a soft logout
+                        softLogout();
+                    } catch (e) {
+                        /* aborted, stop got called */
+                    }
+                }
+            } catch (e) {
+                /* aborted, stop got called */
+            }
+        }
+    }
+
+    /**
+     * Stop tracking user activity
+     */
+    public stop(): void {
+        if (this.dispatcherRef) {
+            dis.unregister(this.dispatcherRef);
+            this.dispatcherRef = null;
+        }
+        if (this.unavailableTimer) {
+            this.unavailableTimer.abort();
+            this.unavailableTimer = null;
+        }
+    }
+
+    /**
+     * Get the current presence state.
+     * @returns {string} the presence state (see PRESENCE enum)
+     */
+    public getState(): State | null {
+        return this.state;
+    }
+
+    private onAction = (payload: ActionPayload): void => {
+        if (payload.action === "user_activity") {
+            this.setState(State.Online);
+            this.unavailableTimer?.restart();
+            this.autoLogoutTimer?.restart();
+        }
+    };
+
+    /**
+     * Set the presence state.
+     * If the state has changed, the homeserver will be notified.
+     * @param {string} newState the new presence state (see PRESENCE enum)
+     */
+    private async setState(newState: State): Promise<void> {
+        if (newState === this.state) {
+            return;
+        }
+
+        const oldState = this.state;
+        this.state = newState;
+
+        if (MatrixClientPeg.safeGet().isGuest()) {
+            return; // don't try to set presence when a guest; it won't work.
+        }
+
+        try {
+            await MatrixClientPeg.safeGet().setPresence({ presence: this.state });
+            logger.debug("Presence:", newState);
+        } catch (err) {
+            logger.error("Failed to set presence:", err);
+            this.state = oldState;
+        }
+    }
+}
+
+export default new Presence();
